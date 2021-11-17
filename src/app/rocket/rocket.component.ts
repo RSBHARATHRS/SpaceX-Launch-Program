204614eee5e973079e626b2103296eb70ebd56a5
Bharath R <78311020+RSBHARATHRS@users.noreply.github.com>
2021-11-17 22:42:17 +0530

    added some paddings

--- src/app/rocket/rocket.component.ts
+++ src/app/rocket/rocket.component.ts
@@ -8,47 +8,47 @@ import { SpaceXService } from '../space-x.service';
 })
 export class RocketComponent implements OnInit {
 
-  years:any;
-  rockets:any;
+  years: any;
+  rockets: any;
 
-  developerName:string;
-  previousSelectedYearBtn:any;
-  yearBtnClick:any;
-  succLanchClick:any;
+  developerName: string;
+  previousSelectedYearBtn: any;
+  yearBtnClick: any;
+  succLanchClick: any;
 
   constructor(private spaceX: SpaceXService) {
-    this.years =[2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020];
-    this.rockets=[];
+    this.years = [2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020];
+    this.rockets = [];
     this.developerName = "Bharath";
     this.previousSelectedYearBtn = 0;
-    this.yearBtnClick = [false, false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false];
-    this.succLanchClick = true;
+    this.yearBtnClick = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
+
   }
 
   ngOnInit(): void {
-    this.spaceX.fetchRockets().subscribe(data=>{
+    this.spaceX.fetchRockets().subscribe(data => {
       this.rockets = data;
     });
   }
 
-  yearFilter(year:any){
+  yearFilter(year: any) {
     this.yearBtnClick[this.previousSelectedYearBtn] = false;
 
-    this.spaceX.filterByYear(year).subscribe(data=>{
+    this.spaceX.filterByYear(year).subscribe(data => {
       this.rockets = data;
     })
     this.yearBtnClick[year - 2006] = true;
     this.previousSelectedYearBtn = year - 2006;
   }
 
-  launchSuccessFilter(boolValue:any){
-    this.spaceX.filterBySuccLaunch(boolValue).subscribe(data=>{
-      this.rockets=data;
+  launchSuccessFilter(boolValue: any) {
+    this.spaceX.filterBySuccLaunch(boolValue).subscribe(data => {
+      this.rockets = data;
     })
   }
 
-  landSuccessFilter(boolValue:any){
-    this.spaceX.filterBySuccLand(boolValue).subscribe(data=>{
+  landSuccessFilter(boolValue: any) {
+    this.spaceX.filterBySuccLand(boolValue).subscribe(data => {
       this.rockets = data;
     })
   }
